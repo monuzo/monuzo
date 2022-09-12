0b80f0187074c9477cc6b4755f9c574651417764
monuzo <113219885+monuzo@users.noreply.github.com>
2022-09-13 06:30:57 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Monuzo',
+  titleSuffix: ' – Documentation',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Monuzo</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Game Cheats
+        Game Cheats Documentation
       </span>
     </>
   ),
