4d1c91c3ac2dc8efc1b7ada6aed07ccfd8e7bf42
monuzo <113219885+monuzo@users.noreply.github.com>
2022-09-13 06:32:32 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -60,7 +60,7 @@ export default {
   search: true,
   prevLinks: true,
   nextLinks: true,
-  footer: true,
+  footer: false,
   footerEditLink: 'Edit this page on GitHub',
   footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
   unstable_faviconGlyph: '👋',
